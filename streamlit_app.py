893845f2d77ca5bb568cae2225ead2686d02e2f3
Sourith Dey <sankardey1121990@gmail.com>
2026-08-31 21:29:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ for message in st.session_state.messages:
     st.chat_message(message["role"]).write(message["content"])
 user_input=st.chat_input("Ask a Question...")
 if user_input:
-    q=user_input.lower().strip()
+    q=user_input.strip()
     if q.startswith(("who is","who was","what is","what was","define")):
         instruction="- Answer in one sentence."
     else:
